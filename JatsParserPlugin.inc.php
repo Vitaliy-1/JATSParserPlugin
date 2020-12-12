a005a2b793dda59712776cbc7f78ad70fb7cdf8e
Vitaliy Bezsheiko <vitaliybezsh@gmail.com>
2020-12-12 22:02:32 +0200

    #41 PDF styling

--- JatsParserPlugin.inc.php
+++ JatsParserPlugin.inc.php
@@ -222,7 +222,7 @@ class JatsParserPlugin extends GenericPlugin {
 	 * @param $issue Issue
 	 * @param
 	 */
-	private function pdfCreation(string $fulltext, Publication $publication, Request $request, string $localeKey): string
+	private function pdfCreation(string $htmlString, Publication $publication, Request $request, string $localeKey): string
 	{
 		// HTML preparation
 		$context = $request->getContext(); /* @var $context Journal */
@@ -322,10 +322,66 @@ class JatsParserPlugin extends GenericPlugin {
 		$pdfDocument->setCellPaddings(0, 0, 0, 0);
 		$pdfDocument->SetFont('dejavuserif', '', 10);
 
-		$htmlString = $fulltext;
+		$htmlString .= "\n" . '<style>' . "\n" . file_get_contents($this->getPluginPath() . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'styles' . DIRECTORY_SEPARATOR . 'default' . DIRECTORY_SEPARATOR . 'pdfGalley.css') . '</style>';
+		$htmlString = $this->_prepareForPdfGalley($htmlString);
+		error_log($htmlString);
 		$pdfDocument->writeHTML($htmlString, true, false, true, false, '');
 
 		return $pdfDocument->Output('article.pdf', 'S');
+	}
+
+	/**
+	 * @param string $htmlString
+	 * @return string Preprocessed HTML string for TCPDF
+	 */
+	private function _prepareForPdfGalley(string $htmlString): string {
+
+		$dom = new DOMDocument('1.0', 'utf-8');
+		$htmlHead = "\n";
+		$htmlHead .= '<head>';
+		$htmlHead .= "\t" . '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>';
+		$htmlHead .= "\n";
+		$htmlHead .= '</head>';
+		$dom->loadHTML($htmlHead . $htmlString);
+
+		// set style for figures and table
+		$xpath = new \DOMXPath($dom);
+
+		$tableNodes = $xpath->evaluate('//table');
+		foreach ($tableNodes as $tableNode) {
+			$tableNode->setAttribute('border', '1');
+			$tableNode->setAttribute('cellpadding', '2');
+		}
+
+		$captionNodes = $xpath->evaluate('//figure/p[@class="caption"]|//table/caption');
+		foreach ($captionNodes as $captionNode) {
+			$captionParts = $xpath->evaluate('span[@class="label"]|span[@class="title"]', $captionNode);
+			foreach ($captionParts as $captionPart) {
+				$emptyTextNode = $dom->createTextNode(' ');
+				$captionPart->appendChild($emptyTextNode);
+			}
+		}
+
+		// TCPDF doesn't recognize display property, insert div
+		$tableCaptions = $xpath->evaluate('//table/caption');
+		foreach ($tableCaptions as $tableCaption) {
+			/* @var $tableNode \DOMNode */
+			$tableNode = $tableCaption->parentNode;
+			$divNode = $dom->createElement('div');
+			$divNode->setAttribute('class', 'caption');
+			$nextToTableNode = $tableNode->nextSibling;
+			if ($nextToTableNode) {
+				$tableNode->parentNode->insertBefore($divNode, $nextToTableNode);
+			}
+			$divNode->appendChild($tableCaption);
+		}
+
+		// Remove redundant whitespaces before caption label
+		$modifiedHtmlString = $dom->saveHTML();
+		$modifiedHtmlString = preg_replace('/<caption>\s*/', '<br>' . '<caption>', $modifiedHtmlString);
+		$modifiedHtmlString = preg_replace('/<p class="caption">\s*/', '<p class="caption">', $modifiedHtmlString);
+
+		return $modifiedHtmlString;
 	}
 
 	/**
