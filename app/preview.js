684dacea5f82819186f4fe7df0723aaeb908aafc
Vitaliy Bezsheiko <vitaliybezsh@gmail.com>
2020-12-14 17:24:59 +0200

    #33 Remove the link to the production ready files

--- app/preview.js
+++ app/preview.js
@@ -1,18 +1,4 @@
 $(document).ready(function () {
-	$(window).on("load", function() {
-		// Create a link to the production ready files
-		var workflowButton = $("#workflow-button");
-		var linkToProduction = $("#jatsParser__linkToProduction");
-		var productionButton = $("#ui-id-7");
-		
-		linkToProduction.click(function () {
-			workflowButton.trigger("click");
-			productionButton.trigger("click");
-		});
-	});
-});
-
-$(document).ready(function () {
 	// Create a link to preview based on checked option
 	var linkToPreview = $("#jatsParser__linkPreview");
 	var baseUrl = linkToPreview.attr("href");
